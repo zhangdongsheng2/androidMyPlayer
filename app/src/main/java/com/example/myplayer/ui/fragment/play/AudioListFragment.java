896f71215fc050cad0255254a8378ae11a1b3baa
zhangdongsheng <zhangdongsheng@sina.com>
2016-08-10 15:33:44 +0800

    获取网络信息

--- app/src/main/java/com/example/myplayer/ui/fragment/play/AudioListFragment.java
+++ app/src/main/java/com/example/myplayer/ui/fragment/play/AudioListFragment.java
@@ -1,15 +1,23 @@
 package com.example.myplayer.ui.fragment.play;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myplayer.R;
 import com.example.myplayer.base.BaseFragment;
+import com.example.myplayer.util.SIMCardNetInfo;
 import com.example.myplayer.util.ViewUtils;
 
 /**
  * Created by Administrator on 2016/3/31.
  */
 public class AudioListFragment extends BaseFragment {
+    private Button button_getSIMInfo;
+    private TextView number;
+    private TextView privoid;
+    private TextView netState;
+
     @Override
     protected View initView() {
         return ViewUtils.inflateView(R.layout.fragment_audio);
@@ -17,6 +25,26 @@ public class AudioListFragment extends BaseFragment {
 
     @Override
     protected void initListener() {
+        button_getSIMInfo = (Button) mRootView.findViewById(R.id.getSIMInfo);
+        number = (TextView) mRootView.findViewById(R.id.textView1);
+        privoid = (TextView) mRootView.findViewById(R.id.textView2);
+        netState = (TextView) mRootView.findViewById(R.id.textView3);
+        button_getSIMInfo.setOnClickListener(new ButtonListener());
+    }
+
+    class ButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (v == button_getSIMInfo) {
+                SIMCardNetInfo siminfo = new SIMCardNetInfo(getActivity());
+                System.out.println(siminfo.getProvidersName());
+                System.out.println(siminfo.getNativePhoneNumber());
+                number.setText(siminfo.getNativePhoneNumber());
+                privoid.setText(siminfo.getProvidersName());
+                netState.setText(siminfo.getNetworkState());
+            }
+        }
 
     }
 
---
+++ app/src/main/java/com/example/myplayer/util/SIMCardNetInfo.java
@@ -0,0 +1,148 @@
+package com.example.myplayer.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+/**
+ * class name：SIMCardNetInfo<BR>
+ * class description：读取Sim卡信息<BR> 获取网络类型
+ * PS： 必须在加入各种权限 <BR>
+ * Date:2012-3-12<BR>
+ *
+ * @author CODYY)peijiangping
+ * @version 1.00
+ */
+public class SIMCardNetInfo {
+    /**
+     * TelephonyManager提供设备上获取通讯服务信息的入口。 应用程序可以使用这个类方法确定的电信服务商和国家 以及某些类型的用户访问信息。
+     * 应用程序也可以注册一个监听器到电话收状态的变化。不需要直接实例化这个类
+     * 使用Context.getSystemService(Context.TELEPHONY_SERVICE)来获取这个类的实例。
+     */
+    private TelephonyManager telephonyManager;
+    /**
+     * 国际移动用户识别码
+     */
+    private String IMSI;
+    private static Context mContext;
+
+    public SIMCardNetInfo(Context context) {
+        mContext = context;
+        telephonyManager = (TelephonyManager) context
+                .getSystemService(Context.TELEPHONY_SERVICE);
+    }
+
+    /**
+     * Role:获取当前设置的电话号码
+     * <BR>Date:2012-3-12
+     * <BR>@author CODYY)peijiangping
+     */
+    public String getNativePhoneNumber() {
+        String NativePhoneNumber = null;
+        NativePhoneNumber = telephonyManager.getLine1Number();
+        return NativePhoneNumber;
+    }
+
+    /**
+     * Role:Telecom service providers获取手机服务商信息 <BR>
+     * 需要加入权限<uses-permission
+     * android:name="android.permission.READ_PHONE_STATE"/> <BR>
+     * Date:2012-3-12 <BR>
+     *
+     * @author CODYY)peijiangping
+     */
+    public String getProvidersName() {
+        String ProvidersName = null;
+        // 返回唯一的用户ID;就是这张卡的编号神马的
+        IMSI = telephonyManager.getSubscriberId();
+        // IMSI号前面3位460是国家，紧接着后面2位00 02是中国移动，01是中国联通，03是中国电信。
+        System.out.println(IMSI);
+        if (IMSI.startsWith("46000") || IMSI.startsWith("46002")) {
+            ProvidersName = "中国移动";
+        } else if (IMSI.startsWith("46001")) {
+            ProvidersName = "中国联通";
+        } else if (IMSI.startsWith("46003")) {
+            ProvidersName = "中国电信";
+        }
+        return ProvidersName;
+    }
+
+    //=========================== 获取网络信息=========================================
+    public static final String NETWORN_NONE = "没有网络";
+    public static final String NETWORN_WIFI = "WiFi";
+    public static final String NETWORN_2G = "2G网络";
+    public static final String NETWORN_3G = "3G网络";
+    public static final String NETWORN_4G = "4G网络";
+    public static final String NETWORN_MOBILE = "Web网络";
+
+    /**
+     * 获取当前网络连接类型
+     *
+     * @param
+     * @return
+     */
+    public static String getNetworkState() {
+        //获取系统的网络服务
+        ConnectivityManager connManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        //如果当前没有网络
+        if (null == connManager)
+            return NETWORN_NONE;
+        //获取当前网络类型，如果为空，返回无网络
+        NetworkInfo activeNetInfo = connManager.getActiveNetworkInfo();
+        if (activeNetInfo == null || !activeNetInfo.isAvailable()) {
+            return NETWORN_NONE;
+        }
+        // 判断是不是连接的是不是wifi
+        NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (null != wifiInfo) {
+            NetworkInfo.State state = wifiInfo.getState();
+            if (null != state)
+                if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
+                    return NETWORN_WIFI;
+                }
+        }
+        // 如果不是wifi，则判断当前连接的是运营商的哪种网络2g、3g、4g等
+        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (null != networkInfo) {
+            NetworkInfo.State state = networkInfo.getState();
+            String strSubTypeName = networkInfo.getSubtypeName();
+            if (null != state)
+                if (state == NetworkInfo.State.CONNECTED || state == NetworkInfo.State.CONNECTING) {
+                    switch (activeNetInfo.getSubtype()) {
+                        //如果是2g类型
+                        case TelephonyManager.NETWORK_TYPE_GPRS: // 联通2g
+                        case TelephonyManager.NETWORK_TYPE_CDMA: // 电信2g
+                        case TelephonyManager.NETWORK_TYPE_EDGE: // 移动2g
+                        case TelephonyManager.NETWORK_TYPE_1xRTT:
+                        case TelephonyManager.NETWORK_TYPE_IDEN:
+                            return NETWORN_2G;
+                        //如果是3g类型
+                        case TelephonyManager.NETWORK_TYPE_EVDO_A: // 电信3g
+                        case TelephonyManager.NETWORK_TYPE_UMTS:
+                        case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                        case TelephonyManager.NETWORK_TYPE_HSDPA:
+                        case TelephonyManager.NETWORK_TYPE_HSUPA:
+                        case TelephonyManager.NETWORK_TYPE_HSPA:
+                        case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                        case TelephonyManager.NETWORK_TYPE_EHRPD:
+                        case TelephonyManager.NETWORK_TYPE_HSPAP:
+                            return NETWORN_3G;
+                        //如果是4g类型
+                        case TelephonyManager.NETWORK_TYPE_LTE:
+                            return NETWORN_4G;
+                        default:
+                            //中国移动 联通 电信 三种3G制式
+                            if (strSubTypeName.equalsIgnoreCase("TD-SCDMA") || strSubTypeName.equalsIgnoreCase("WCDMA") || strSubTypeName.equalsIgnoreCase("CDMA2000")) {
+                                return NETWORN_3G;
+                            } else {
+                                return NETWORN_MOBILE;
+                            }
+                    }
+                }
+        }
+        return NETWORN_NONE;
+    }
+
+
+}
